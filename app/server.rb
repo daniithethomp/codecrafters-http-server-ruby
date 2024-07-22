53650dd04ac60be788181c0630e86900d57790a4
danii thompson <danii.thompson8@gmail.com>
2024-07-22 22:27:05 +0100

    codecrafters submit [skip ci]

--- app/server.rb
+++ app/server.rb
@@ -6,6 +6,16 @@ print("Logs from your program will appear here!")
 server = TCPServer.new("localhost", 4221)
 client_socket, client_address = server.accept
 
+known_paths = ["/"]
+
 while line = client_socket.gets
-    client_socket.puts "HTTP/1.1 200 OK\r\n\r\n"
+    method, path, version = line.split
+    puts method
+    puts path
+    puts version
+    if known_paths.include? path then
+        client_socket.puts "HTTP/1.1 200 OK\r\n\r\n"
+    else
+        client_socket.puts "HTTP/1.1 404 Not Found\r\n\r\n"
+    end
 end
