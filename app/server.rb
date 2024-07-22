f3405f84a90b4752a98f44e67e68dbfb46fc0ab6
danii thompson <danii.thompson8@gmail.com>
2024-07-22 23:34:19 +0100

    codecrafters submit [skip ci]

--- app/server.rb
+++ app/server.rb
@@ -1,24 +1,68 @@
 require "socket"
 
-# You can use print statements as follows for debugging, they'll be visible when running tests.
-print("Logs from your program will appear here!")
+HTTP_STATUS = {200 => "OK", 404 => "Not Found" }
 
 server = TCPServer.new("localhost", 4221)
 client_socket, client_address = server.accept
 
-known_paths = ["/","echo"]
+def read_lines(client)
+    lines = []
+    while (line = client.gets) != "\r\n"
+        lines << line.chomp
+    end
+    lines
+end
 
-while line = client_socket.gets
-    puts line.split
-    method, path, version = line.split
-    if known_paths.include? path then
-        client_socket.puts "HTTP/1.1 200 OK\r\n\r\n"
-    elsif known_paths.include? path.split("/")[1] then
-        status = "HTTP/1.1 200 OK\r\n"
-        body = path.split("/").last.strip
-        header = "Content-Type: text/plain\r\nContent-Length: #{body.length}\r\n\r\n"
-        client_socket.puts "#{status}#{header}#{body}"
+def parse_request(request_lines)
+    method, path, version = request_lines.first.split
+    request_hash = { method: method, path: path }
+    request_lines.drop(1).each do |header|
+        key, value = header.split(":", 2).map(&:strip)
+        request_hash[key] = value
+    end
+    request_hash
+end
+
+def generate_response(request)
+    method = request[:method]
+    path = request[:path]
+    headers = { "Content-Type" => "text/plain" }
+    case[method, path]
+    in ["GET", "/"]
+        [200, headers, []]
+    in ["GET",%r{^/echo/(.*)$}]
+        echo_message = path.split("/").last
+        headers["Content-Length"] = echo_message.length.to_s
+        [200, headers, echo_message]
+    in ["GET", "/user-agent"]
+        body = request.fetch("User-Agent","")
+        headers["Content-Length"] = body.length.to_s
+        [200,headers,body]
     else
-        client_socket.puts "HTTP/1.1 404 Not Found\r\n\r\n"
+        [404, headers, []]
     end
 end
+
+def pretty_response(response)
+    puts response
+    status, headers, body = response
+    str = "HTTP/1.1 #{status} #{HTTP_STATUS[status]}"
+    str += "\r\n"
+    headers.each do |key, value|
+        str += "#{key}: #{value}"
+        str += "\r\n"
+    end
+    str += "\r\n"
+    unless body.empty? then
+        str += body
+        str += "\r\n"
+    end
+    str
+end
+            
+request_lines = read_lines(client_socket)
+headers = parse_request(request_lines)
+response = generate_response(headers)
+client_socket.puts pretty_response(response)
+
+client_socket.close
